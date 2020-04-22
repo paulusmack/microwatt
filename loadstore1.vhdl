bc7d32323bf3062d4a482cd76fd6496db7e9a27c
Paul Mackerras <paulus@ozlabs.org>
2020-04-22 16:53:39 +1000

    Pass mtspr/mfspr to MMU-related SPRs down to loadstore1
    This arranges for some mfspr and mtspr to get sent to loadstore1
    instead of being handled in execute1.  In particular, DAR and DSISR
    are handled this way.  They are therefore "slow" SPRs.
    
    While we're at it, fix the spelling of HEIR and remove mention of
    DAR and DSISR from the comments in execute1.
    
    Signed-off-by: Paul Mackerras <paulus@ozlabs.org>

--- loadstore1.vhdl
+++ loadstore1.vhdl
@@ -61,6 +61,8 @@ architecture behave of loadstore1 is
         virt_mode    : std_ulogic;
         state        : state_t;
         second_bytes : std_ulogic_vector(7 downto 0);
+        dar          : std_ulogic_vector(63 downto 0);
+        dsisr        : std_ulogic_vector(31 downto 0);
     end record;
 
     type byte_sel_t is array(0 to 7) of std_ulogic;
@@ -137,6 +139,9 @@ begin
         variable use_second : byte_sel_t;
         variable trim_ctl : trim_ctl_t;
         variable negative : std_ulogic;
+        variable mfspr : std_ulogic;
+        variable sprn : std_ulogic_vector(9 downto 0);
+        variable sprval : std_ulogic_vector(63 downto 0);
     begin
         v := r;
         req := '0';
@@ -144,6 +149,8 @@ begin
         done := '0';
         byte_sel := (others => '0');
         addr := lsu_sum;
+        mfspr := '0';
+        sprval := (others => '0');      -- avoid inferred latches
 
         write_enable := '0';
         do_update := '0';
@@ -202,15 +209,42 @@ begin
             if l_in.valid = '1' then
                 v.load := '0';
                 v.dcbz := '0';
-                if l_in.op = OP_LOAD then
-                    v.load := '1';
-                elsif l_in.op = OP_DCBZ then
-                    v.dcbz := '1';
-                end if;
                 v.tlbie := '0';
-                if l_in.op = OP_TLBIE then
+                case l_in.op is
+                when OP_STORE =>
+                    req := '1';
+                when OP_LOAD =>
+                    req := '1';
+                    v.load := '1';
+                when OP_DCBZ =>
+                    req := '1';
+                    v.dcbz := '1';
+                when OP_TLBIE =>
+                    req := '1';
                     v.tlbie := '1';
-                end if;
+                when OP_MFSPR =>
+                    done := '1';
+                    mfspr := '1';
+                    -- partial decode on SPR number should be adequate given
+                    -- the restricted set that get sent down this path
+                    sprn := std_ulogic_vector(to_unsigned(l_in.spr_num, 10));
+                    if sprn(0) = '0' then
+                        sprval := x"00000000" & r.dsisr;
+                    else
+                        sprval := r.dar;
+                    end if;
+                when OP_MTSPR =>
+                    done := '1';
+                    sprn := std_ulogic_vector(to_unsigned(l_in.spr_num, 10));
+                    if sprn(0) = '0' then
+                        v.dsisr := l_in.data(31 downto 0);
+                    else
+                        v.dar := l_in.data;
+                    end if;
+                when others =>
+                    assert false report "unknown op sent to loadstore1";
+                end case;
+
                 v.addr := lsu_sum;
                 v.write_reg := l_in.write_reg;
                 v.length := l_in.length;
@@ -256,12 +290,13 @@ begin
                     v.store_data(j + 7 downto j) := l_in.data(i * 8 + 7 downto i * 8);
                 end loop;
 
-                req := '1';
-                stall := '1';
-                if long_sel(15 downto 8) = "00000000" then
-                    v.state := LAST_ACK_WAIT;
-                else
-                    v.state := SECOND_REQ;
+                if req = '1' then
+                    stall := '1';
+                    if long_sel(15 downto 8) = "00000000" then
+                        v.state := LAST_ACK_WAIT;
+                    else
+                        v.state := SECOND_REQ;
+                    end if;
                 end if;
             end if;
 
@@ -320,7 +355,11 @@ begin
         -- Multiplex either cache data to the destination GPR or
         -- the address for the rA update.
         l_out.valid <= done;
-        if do_update = '1' then
+        if mfspr = '1' then
+            l_out.write_enable <= '1';
+            l_out.write_reg <= l_in.write_reg;
+            l_out.write_data <= sprval;
+        elsif do_update = '1' then
             l_out.write_enable <= '1';
             l_out.write_reg <= r.update_reg;
             l_out.write_data <= r.addr;
